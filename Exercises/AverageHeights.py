a3936905e934e33cc8af3ab908a28df02f40a691
Emil-F <e.fagerjord@gmail.com>
2022-06-23 04:43:07 +0200

    Hangman

--- Exercises/AverageHeights.py
+++ Exercises/AverageHeights.py
@@ -17,6 +17,6 @@ number_of_students = 0
 for student in student_heights:
   number_of_students += 1
 
-average_height = total_height / number_of_students
-average_height = round(average_height)
+average_height = round(total_height / number_of_students)
+# average_height = round(average_height)
 print(average_height)
---
+++ hangman.py
@@ -0,0 +1,26 @@
+import random
+
+word_list = random.choice(["Monkey", "Sussy", "Pussy"]).lower()
+print(word_list)
+length = len(word_list)
+display = []
+for i in range(length):
+    display.append("_")
+
+end_of_game = False
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    for position in range(length):
+        letter = word_list[position]
+        if letter == guess:
+            display[position] = guess
+
+    full = (f"\n{' '.join(display)}")
+
+    if "_" not in display:
+        end_of_game = True
+        print("_____________")
+        print("\nSuccess")
+
+    print(full)
