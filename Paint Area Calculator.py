8c5447828243d376ce40db855ef810d54394f619
Emil-F <e.fagerjord@gmail.com>
2022-06-24 22:07:47 +0200

    Paint Area Calculator

---
+++ Paint Area Calculator.py
@@ -0,0 +1,16 @@
+#Write your code below this line 👇
+def paint_calc(height, width, cover):
+    sum = test_h * test_w
+    calc = (sum // coverage) + (sum % coverage > 0)
+    print(f"You'll need {calc} cans of paint.")
+
+
+#Write your code above this line 👆
+# Define a function called paint_calc() so that the code below works.   
+
+# 🚨 Don't change the code below 👇
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
+
